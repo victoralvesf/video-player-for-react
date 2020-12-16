317c75a815fb0fe728f3bc79f9a52c45eac05869
victor <victoralves@fortics.com.br>
2020-12-16 03:04:41 -0300

    chore: add configs to project

--- example/src/App.js
+++ example/src/App.js
@@ -3,8 +3,20 @@ import React from 'react'
 import { ExampleComponent } from 'video-player-for-react'
 import 'video-player-for-react/dist/index.css'
 
+const videoURL =
+  'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4'
+const videoPoster =
+  'https://images.anandtech.com/doci/10729/ChromecastLaunch.jpg'
+
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <ExampleComponent
+      width='928px'
+      url={videoURL}
+      type='video/mp4'
+      poster={videoPoster}
+    />
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,165 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React, { useState, useRef, useEffect } from 'react'
+import './styles.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+let mouseIsPressed = false
+const defaultWidth = document.querySelector('html').offsetWidth * 0.6 + 'px'
+
+function usePlayerState($videoPlayer) {
+  const [playerStatus, setPlayerStatus] = useState({
+    playing: false,
+    currentPercentage: 0,
+    stopped: true
+  })
+
+  useEffect(() => {
+    playerStatus.playing ? playAndPause('play') : playAndPause('pause')
+  }, [$videoPlayer, playerStatus.playing])
+
+  function playAndPause(status) {
+    $videoPlayer.current[status]()
+
+    status === 'play' &&
+      setPlayerStatus({
+        ...playerStatus,
+        stopped: false
+      })
+  }
+
+  function toggleVideoPlay() {
+    setPlayerStatus({
+      ...playerStatus,
+      playing: !playerStatus.playing
+    })
+  }
+
+  function handleInputMouseDown() {
+    mouseIsPressed = true
+  }
+
+  function handleInputMouseUp() {
+    mouseIsPressed = false
+
+    const duration = $videoPlayer.current.duration
+
+    $videoPlayer.current.currentTime =
+      (duration / 100) * playerStatus.currentPercentage
+  }
+
+  function handleTimeUpdate() {
+    if (!mouseIsPressed) {
+      const currentTime = $videoPlayer.current.currentTime
+      const duration = $videoPlayer.current.duration
+
+      const percentage = (currentTime / duration) * 100
+
+      setPlayerStatus({
+        ...playerStatus,
+        currentPercentage: percentage
+      })
+    }
+  }
+
+  function handleChangePercentage(event) {
+    const currentPercentageValue = event.target.value
+
+    setPlayerStatus({
+      ...playerStatus,
+      currentPercentage: currentPercentageValue
+    })
+  }
+
+  function handleResetVideo() {
+    setPlayerStatus({
+      ...playerStatus,
+      playing: false,
+      currentPercentage: 0,
+      stopped: true
+    })
+  }
+
+  function isPlayerStopped() {
+    if (playerStatus.stopped && playerStatus.currentPercentage === 0) {
+      return 'vpfr_stopped_player'
+    } else if (playerStatus.playing) {
+      return 'vpfr_hide_controls'
+    } else {
+      return 'vpfr_paused_player'
+    }
+  }
+
+  return {
+    playerStatus,
+    toggleVideoPlay,
+    handleTimeUpdate,
+    handleChangePercentage,
+    handleInputMouseDown,
+    handleInputMouseUp,
+    handleResetVideo,
+    isPlayerStopped
+  }
+}
+
+export const ExampleComponent = ({ width, height, url, type, poster }) => {
+  const $videoPlayer = useRef(null)
+  const {
+    playerStatus,
+    toggleVideoPlay,
+    handleTimeUpdate,
+    handleChangePercentage,
+    handleInputMouseDown,
+    handleInputMouseUp,
+    handleResetVideo,
+    isPlayerStopped
+  } = usePlayerState($videoPlayer)
+
+  return (
+    <div className={`vpfr_container ${isPlayerStopped()}`}>
+      <div className='vpfr_player_controls'>
+        <button onClick={toggleVideoPlay}>
+          {playerStatus.playing ? 'Pause' : 'Play'}
+        </button>
+
+        <input
+          type='range'
+          min='0'
+          max='100'
+          onMouseDown={handleInputMouseDown}
+          onChange={handleChangePercentage}
+          onMouseUp={handleInputMouseUp}
+          value={playerStatus.currentPercentage}
+        />
+      </div>
+
+      <div className='vpfr_player_controls_shadow' onClick={toggleVideoPlay} />
+
+      <div className='vpfr_video_wrapper'>
+        <video
+          crossOrigin='true'
+          preload='metadata'
+          width={width || defaultWidth}
+          height={height || undefined}
+          ref={$videoPlayer}
+          onTimeUpdate={handleTimeUpdate}
+          onEnded={handleResetVideo}
+          className='vpfr_video'
+        >
+          <source src={url} type={type} />
+        </video>
+        {poster && (
+          <div
+            className='vpfr_video_poster_image'
+            style={{ backgroundImage: `url(${poster})` }}
+          />
+        )}
+      </div>
+
+      <button
+        className='vpfr_overlay_play_button'
+        onClick={toggleVideoPlay}
+        style={{ display: playerStatus.playing ? 'none' : 'block' }}
+      >
+        Play
+      </button>
+    </div>
+  )
 }
